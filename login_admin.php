4de697f007d537b759e55626632339970bfe2bcc
Ghani <23_muhammadsyifazai@student.smkti.net>
2025-05-22 08:23:18 +0800

    Add files via upload

--- login_admin.php
+++ login_admin.php
@@ -31,7 +31,7 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Admin</title>
-    <link rel="stylesheet" href="style.css"
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
